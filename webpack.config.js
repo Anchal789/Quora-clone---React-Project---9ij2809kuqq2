aab47fc62aa847870170561f5321b4f13a341c1d
Anchal Deshmukh <anchaldeshmukh789@gmail.com>
2023-07-21 01:25:17 +0530

    success

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,56 @@
-import { join } from "path";
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-export const entry = './src/index.js';
-export const output = {
-    path: join(__dirname, "/dist"),
-    filename: "index_bundle.js",
-};
-export const module = {
-    rules: [
-        {
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            use: [
-                {
-                    loader: 'file-loader',
-                },
-            ],
-        },
-        {
-            test: /\.js$|\.jsx$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.js$|\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
-        },
-        {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: 'style-loader',
-                },
-                {
-                    loader: 'css-loader',
-                },
-            ]
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                'css-loader',
-                'sass-loader'
-            ]
-        }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ],
-};
-export const plugins = [
-    new HtmlWebpackPlugin({
-        template: "./src/index.html"
-    })
-];
-export const resolve = {
-    extensions: ['.js', '.jsx'],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
 };
